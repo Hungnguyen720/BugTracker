c63791fd38886451aedce403cfcba69d20970103
Hung Nguyen <hungnguyen720@gmail.com>
2019-12-01 19:14:14 -0800

    Register controller server data validation

--- Data/Models/UserModel.cs
+++ Data/Models/UserModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Data.Models
@@ -7,10 +8,14 @@ namespace Data.Models
     public class User
     {
         public int Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string EmailAddress { get; set; }
         public string ProfilePicture { get; set; }
+
         public string Type { get; set; }
         public string Password { get; set; }
     }
