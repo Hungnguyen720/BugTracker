4cb78cbea5c842f500e7ee4a0b49d93441f90b17
Hung Nguyen <HungNguyen720@gmail.com>
2019-12-03 17:32:39 -0800

    Create Project Component - made on change handlers for inputs

--- BugTracker/ClientApp/src/components/ProjectComponents/ProjectCreateComponent.js
+++ BugTracker/ClientApp/src/components/ProjectComponents/ProjectCreateComponent.js
@@ -7,28 +7,46 @@ import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
 
 export class ProjectCreateComponent extends Component {
+    constructor() {
+        super()
+        this.state = {
+            projectTitle: '',
+            Owner: '',
+            DateStarted: '',
+            DateEnd: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event) {
+        const { name, value } = event.target
+
+        this.setState({
+            [name]: value
+        })
+    }
+
+
     render() {
         return (
             <div>
                 <h1>Create Project</h1>
             <Form>
-
                 <Form.Field>
-                    <label>Project Title</label>
-                    <input />
+                        <label>Project Title</label>
+                        <input name='projectTitle' value={this.state.projectTitle} onChange={this.handleChange} />
                 </Form.Field>
                 <Form.Field>
-                    <label>Owner</label>
-                    <input />
+                        <label>Owner</label>
+                        <input name='Owner' value={this.state.Owner} onChange={this.handleChange}/>
                 </Form.Field>
-
-                <Form.Group widths='equal' >
-                    <SemanticDatepicker />
-                    <SemanticDatepicker />
+                    <Form.Group widths='equal' >
+                        <SemanticDatepicker name='DateStarted' />
+                        <SemanticDatepicker name='DateEnd' />
                 </Form.Group>
                 <Editor
                 />
-
                 </Form>
                 </div>
         )
